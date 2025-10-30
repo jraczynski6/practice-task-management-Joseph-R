30edeac50e6b01fa1e005aee281567e0c9f89206
jraczynski <raczynski.joseph616@gmail.com$ git config --global user.email raczynski.joseph616@gmail.com>
2025-10-29 22:10:21 -0500

    Fix TaskList.jsx Link

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,46 +1,19 @@
 import TaskDetail from "./TaskDetail";
 import { Link } from "react-router";
 
-const TaskList = () => {
-
-    const taskData = [
-        {
-            id: 1,
-            title: 'Complete lesson',
-            description: 'React Lesson'
-        },
-        {
-            id: 2,
-            title: 'Complete group practice',
-            description: 'React State Management and Event Handling - 4 - Task Management'
-        },
-        {
-            id: 3,
-            title: 'Start on project',
-            description: 'Unit 1 Final Project - React App'
-        }
-    ];
-    
-    
+export default function TaskList({tasks}) {
+    console.log(tasks);
     return (
         <div>
             <h2>Task list</h2>
             <ul>
-                {taskData.map((task) => {
-                    return (
-                        <li key={task.id}>
-                            <Link to={"/details" + task.id} key={task} >
-                            {task.title}
-                            </Link>
-                           </li>                        
-                    );
-                })}
+                {tasks.map(task => (
+                    <li key={task.id}>
+                        <Link to={`/details/${task.id}`}>{task.title}</Link>
+                    </li>
+        ))}
             </ul>
-                {/* <TaskDetail task={taskData} /> */}
-                <TaskDetail task={taskData} />
         </div>
     );
 
 };
-
-export default TaskList;
